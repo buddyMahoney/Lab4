ec4e4b7bdc670a22c8a9d62936ee7b09d1197e2c
buddyMahoney <wmahoney@carthage.edu>
2026-05-30 20:24:41 -0500

    Finished the Pixel class, wrote tests in the driver and created the barebones Icon class.

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,26 @@
 public class Driver {
     public static void main(String[] args){
         System.out.println("Welcome to Lab 4!");
+
+        Pixel p1 = new Pixel();
+
+        System.out.println(p1.toString());
+
+        Pixel p2 = new Pixel();
+
+        p2.setRedVal(20);
+        p2.setGreenVal(40);
+        p2.setBlueVal(60);
+
+        System.out.println(p2.toString());
+
+        Pixel p3 = new Pixel();
+
+        p3.setRedVal(6000);
+        p3.setGreenVal(-1);
+        p3.setBlueVal(25);
+
+        System.out.println(p3.toString());
+
     }
 }
---
+++ src/Pixel.java
@@ -0,0 +1,58 @@
+public class Pixel
+{
+    private int redVal;
+    private int greenVal;
+    private int blueVal;
+
+    public Pixel()
+    {
+        this.redVal = 0;
+        this.greenVal = 0;
+        this.blueVal = 0;
+    }
+
+    public String toString()
+    {
+        String pixelString = "Red: " + getRedVal()+ ", Green: " + getGreenVal() + ", Blue: " + getBlueVal();
+        return pixelString;
+    }
+
+    public int getRedVal()
+    {
+        return redVal;
+    }
+
+    public void setRedVal(int rV)
+    {
+        if(rV >= 0 && rV <= 255)
+        {
+            this.redVal = rV;
+        }
+    }
+
+    public int getGreenVal()
+    {
+        return greenVal;
+    }
+
+    public void setGreenVal(int gV)
+    {
+        if(gV >= 0 && gV <= 255)
+        {
+            this.greenVal = gV;
+        }
+    }
+
+    public int getBlueVal()
+    {
+            return blueVal;
+    }
+
+    public void setBlueVal(int bV)
+    {
+        if(bV >= 0 && bV <= 255)
+        {
+            this.blueVal = bV;
+        }
+    }
+}
